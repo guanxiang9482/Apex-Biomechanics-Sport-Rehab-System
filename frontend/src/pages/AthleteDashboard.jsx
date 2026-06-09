eea951d4dc9d6999b163acb6a482b62a08490100
Maxwin14 <maxwin140@gmail.com>
2026-06-09 16:40:07 +0800

    small update

--- frontend/src/pages/AthleteDashboard.jsx
+++ frontend/src/pages/AthleteDashboard.jsx
@@ -1,6 +1,7 @@
 import {useState,useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import {athlete,auth} from'../services/api';
+import './Dashboard.css';
 
 function AthleteDashboard(){
         const [activeSection, setActiveSection] =useState('sessions');
